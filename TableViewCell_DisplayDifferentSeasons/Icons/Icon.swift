64dd80cee4cf7314e22b887dce62d706bb42047e
Pranay Boggarapu <pranayboggarapu@gmail.com>
2018-08-01 20:44:27 -0700

    Commiting from Office System - Initial Commit

---
+++ TableViewCell_DisplayDifferentSeasons/Icons/Icon.swift
@@ -0,0 +1,35 @@
+//
+//  Icon.swift
+//  TableViewCell_DisplayDifferentSeasons
+//
+//  Created by Pranay Boggarapu on 7/26/18.
+//  Copyright © 2018 Pranay Boggarapu. All rights reserved.
+//
+
+import UIKit
+
+enum RatingType: Int {
+    case Unrated
+    case Ugly
+    case OK
+    case Nice
+    case Awesome
+}
+
+class Icon {
+    
+    var title: String
+    var subTitle: String
+    var image: UIImage?
+    var rating = RatingType.Unrated
+    
+    init(withTitle title: String, subTitle: String, imageName: String) {
+        self.title = title
+        self.subTitle = subTitle
+        if let imageName = imageName {
+            if let iconImage = UIImage(named: imageName) {
+                image = iconImage
+            }
+        }
+    }
+}
