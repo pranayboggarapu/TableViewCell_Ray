3780f64158402087a0ecde409d07e60f897f3cca
Pranay Boggarapu <pranayboggarapu@gmail.com>
2018-08-07 07:22:38 -0700

    Implemented addition methods

--- TableViewCell_DisplayDifferentSeasons/ViewController.swift
+++ TableViewCell_DisplayDifferentSeasons/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib
         IconSets = IconSet.iconSets()
         navigationItem.rightBarButtonItem = editButtonItem
-        
+        prettyIconsTableView.allowsSelectionDuringEditing = true
         
     }
 
@@ -30,13 +30,26 @@ class ViewController: UIViewController {
 
 }
 
-extension ViewController: UITableViewDataSource {
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     override func setEditing(_ editing: Bool, animated: Bool) {
         super.setEditing(editing, animated: animated)
+       
         if editing {
+             prettyIconsTableView.beginUpdates()
+            for(index, set) in IconSets.enumerated() {
+                let indexPath = NSIndexPath(item: set.icons.count, section: index)
+                prettyIconsTableView.insertRows(at: [indexPath as IndexPath], with: .automatic)
+            }
+            prettyIconsTableView.endUpdates()
             prettyIconsTableView.setEditing(true, animated: true)
         } else {
+            prettyIconsTableView.beginUpdates()
+            for(index, set) in IconSets.enumerated() {
+                let indexPath = NSIndexPath(item: set.icons.count, section: index)
+                prettyIconsTableView.insertRows(at: [indexPath as IndexPath], with: .automatic)
+            }
+            prettyIconsTableView.endUpdates()
             prettyIconsTableView.setEditing(false, animated: true)
         }
     }
@@ -46,22 +59,34 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return IconSets[section].icons.count
+        
+        let adjustment = isEditing ? 1 : 0
+        
+        
+        return IconSets[section].icons.count + adjustment
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        
         let cell = tableView.dequeueReusableCell(withIdentifier: "iconCell", for: indexPath)
+        let section = IconSets[indexPath.section]
         
-        let icon = IconSets[indexPath.section].icons[indexPath.row]
     
-        
-        cell.textLabel?.text = icon.title
-        cell.detailTextLabel?.text = icon.subTitle
-        if let image = icon.image {
-            cell.imageView?.image = image
+        if indexPath.row >= section.icons.count && isEditing {
+            
+            cell.textLabel?.text = "Add Icon"
+            cell.detailTextLabel?.text = nil
+            cell.imageView?.image = nil
+        } else {
+            let icon = section.icons[indexPath.row]
+            cell.textLabel?.text = icon.title
+            cell.detailTextLabel?.text = icon.subTitle
+            if let image = icon.image {
+                cell.imageView?.image = image
+            } else {
+                cell.imageView?.image = nil
+            }
         }
-        
         return cell
     }
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -78,10 +103,39 @@ extension ViewController: UITableViewDataSource {
             set.icons.remove(at: indexPath.row)
             //tableView.reloadData()
             tableView.deleteRows(at: [indexPath], with: .automatic)
+        }else if editingStyle == .insert {
+            let newIcon = Icon(withTitle: "New Icon", subtitle: "", imageName: nil)
+            let set = IconSets[indexPath.section]
+            set.icons.append(newIcon)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+            
         }
     }
     
     
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        let section = IconSets[indexPath.section]
+        if indexPath.row >= section.icons.count {
+            return .insert
+        }
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        let set = IconSets[indexPath.section]
+        if isEditing && indexPath.row < set.icons.count {
+            return nil
+        }
+        return indexPath
+    }
+ 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        prettyIconsTableView.deselectRow(at: indexPath, animated: true)
+        let set = IconSets[indexPath.section]
+        if indexPath.row >= set.icons.count && isEditing {
+            self.tableView(prettyIconsTableView, commit: .insert, forRowAt: indexPath)
+        }
+    }
     
 }
 
